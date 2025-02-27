23529c99394af368ec718f2b01a1b95884ff0577
ertewi <nikita28984@gmail.com>
2025-02-27 14:20:58 +0700

    Close lab 1

--- lab_1/src/main/java/org/example/Filter.java
+++ lab_1/src/main/java/org/example/Filter.java
@@ -12,50 +12,46 @@ public class Filter {
             while((ch=reader.read())!=-1){
                 if(ch=='\n') {
                     String str = entity;
-//                    boolean isFloat = entity.matches("[0-9]*\\.[0-9]+");
-//                    if(isFloat) {
-//                        writer.writeFloat(str);
-//                        entity = "";
-//                        continue;
-//                    }
-//
-//                    boolean isInt = entity.matches("[0-9]+");
-//                    System.out.println(isInt);
-//                    if(isInt) {
-//                        writer.writeInt(str);
-//                        entity = "";
-//                        continue;
-//                    }
-                    System.out.println(entity);
-
-                    try {
-                        Integer.parseInt(entity);
-                        writer.writeInt(entity);
-                        System.out.println(" int");
+                    boolean isFloat = entity.matches("[0-9]*\\.[0-9]+");
+                    if(isFloat) {
+                        writer.writeFloat(str);
                         entity = "";
                         continue;
-                    } catch (NumberFormatException ex) {
-
                     }
 
-                    try {
-                        Float.parseFloat(entity);
-                        writer.writeFloat(entity);
-
-                        System.out.println(" float");
+                    boolean isInt = entity.matches("[0-9]+");
+                    if(isInt) {
+                        writer.writeInt(str);
                         entity = "";
                         continue;
-                    } catch (NumberFormatException ex) {
-
                     }
 
                     writer.writeString(entity);
-                    System.out.println(" string");
                     entity = "";
                 } else {
+                    if(ch=='\r') {
+                        continue;
+                    }
                     entity += String.valueOf((char) ch);
                 }
             }
+            String str = entity;
+            boolean isFloat = entity.matches("[0-9]*\\.[0-9]+");
+            if(isFloat) {
+                writer.writeFloat(str);
+                entity = "";
+                return;
+            }
+
+            boolean isInt = entity.matches("[0-9]+");
+            if(isInt) {
+                writer.writeInt(str);
+                entity = "";
+                return;
+            }
+
+            writer.writeString(entity);
+            entity = "";
         }
         catch(IOException ex){
 
--- lab_1/src/main/java/org/example/MainParser.java
+++ lab_1/src/main/java/org/example/MainParser.java
@@ -9,7 +9,10 @@ public class MainParser {
     private String extra_path = "";
     private String prefix = "";
     private String stat_format = "none";
-    boolean rewrite_result = true;
+    boolean rewrite_result = false;
+    String result_path = currentPath + File.separator + extra_path + File.separator;
+    Filter filter = new Filter();
+    Writer writer = new Writer(result_path, prefix, rewrite_result);
 
     public void parse(String[] args) {
         int length = args.length;
@@ -23,16 +26,23 @@ public class MainParser {
                 } else {
                     throw new MissingFormatArgumentException("-o requires an argument");
                 }
+                this.result_path = currentPath + File.separator + extra_path + File.separator;
+                this.writer = new Writer(result_path, prefix, rewrite_result);
 
             } else if (arg.equals("-p")) {
                 if (++i < length) {
                     this.prefix = args[i];
                 } else {
-                    throw new MissingFormatArgumentException("-o requires an argument");
+                    throw new MissingFormatArgumentException("-p requires an argument");
                 }
+                this.result_path = currentPath + File.separator + extra_path + File.separator;
+                this.writer = new Writer(result_path, prefix, rewrite_result);
 
             } else if (arg.equals("-a")) {
-                this.rewrite_result = false;
+                this.rewrite_result = true;
+
+                this.result_path = currentPath + File.separator + extra_path + File.separator;
+                this.writer = new Writer(result_path, prefix, rewrite_result);
 
             } else if (arg.equals("-s")) {
                 if (this.stat_format.equals("none")) {
@@ -53,13 +63,10 @@ public class MainParser {
 
                 if (extension != null) {
                     if (extension.equals(".txt")) {
-                        String path = currentPath + File.separator + extra_path;
-                        String file_path = path + arg;
+                        String file_path = currentPath + File.separator;
+                        file_path = file_path + arg;
 
-                        Filter filter = new Filter();
-                        Writer writer = new Writer(path, prefix);
-
-                        filter.filter(file_path, writer);
+                        this.filter.filter(file_path, this.writer);
 
                         continue;
                     }
